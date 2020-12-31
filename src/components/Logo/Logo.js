0dbe13aff97f89d45360f2accaf121353d56b752
VIjayanand Settin <stvijayanand@hotmail.com>
2020-12-30 19:40:37 -0800

    feat: use a logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import burgerLogo from '../../assets/images/burger-logo.png'
+import classes from './Logo.css'
+
+const Logo = (props) => {
+    return (<div className={classes.Logo}>
+        <img src={burgerLogo} alt="MyBurger"></img>
+    </div>)
+}
+
+export default Logo;
